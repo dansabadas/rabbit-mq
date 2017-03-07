7210553ded6065412720fcb46f31766fbb455e7f
AD001\z003p8vs <dan.sabadas@gmail.com>
2017-03-07 20:59:18 +0200

    commit first

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1.StandardQueue;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainStandardQueue.Run();
+        }
+    }
+}
